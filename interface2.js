6fbd95a1d170ca63133fc72850d40fd133a9c792
Alexy <alexandra.fina@yahoo.com>
2018-05-13 13:45:42 +0100

    Update to show Javascript language

---
+++ interface2.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  var website = new FTRatingScore();
+  var log = new FTRatingLog();
+
+  $('#button4').on('click', function() {
+    website.rate(4);
+    log.log(4);
+    $('#score4').text(website.score4);
+    $('#average').text(website.average);
+    $('#percentage4').text(website.percentage4);
+    $('#percentage3').text(website.percentage3);
+    $('#percentage2').text(website.percentage2);
+    $('#percentage1').text(website.percentage1);
+    alert("Thank you for rating FT");
+    console.log(website.score);
+    console.log(log.history);
+  });
+
+  $('#button3').on('click', function() {
+    log.log(3);
+    website.rate(3);
+    $('#score3').text(website.score3);
+    $('#average').text(website.average);
+    $('#percentage4').text(website.percentage4);
+    $('#percentage3').text(website.percentage3);
+    $('#percentage2').text(website.percentage2);
+    $('#percentage1').text(website.percentage1);
+    alert("Thank you for rating FT");
+    console.log(website.score);
+    console.log(log.history);
+  });
+
+  $('#button2').on('click', function() {
+    log.log(2);
+    website.rate(2);
+    $('#score2').text(website.score2);
+    $('#average').text(website.average);
+    $('#percentage4').text(website.percentage4);
+    $('#percentage3').text(website.percentage3);
+    $('#percentage2').text(website.percentage2);
+    $('#percentage1').text(website.percentage1);
+    alert("Thank you for rating FT");
+    console.log(website.score);
+    console.log(log.history);
+  });
+
+  $('#button1').on('click', function() {
+    log.log(1);
+    website.rate(1);
+    $('#score1').text(website.score1);
+    $('#average').text(website.average);
+    $('#percentage4').text(website.percentage4);
+    $('#percentage3').text(website.percentage3);
+    $('#percentage2').text(website.percentage2);
+    $('#percentage1').text(website.percentage1);
+    alert("Thank you for rating FT");
+    console.log(website.score);
+    console.log(log.history);
+  });
+});
